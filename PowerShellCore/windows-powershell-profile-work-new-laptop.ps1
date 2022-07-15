acfd0b289a26a555a4e11164eb1cd9f281205f38
SalimGangji <salim.gangji@gmail.com>
2022-07-15 09:39:09 -0700

    Update windows-powershell-profile-work-new-laptop.ps1

--- PowerShellCore/windows-powershell-profile-work-new-laptop.ps1
+++ PowerShellCore/windows-powershell-profile-work-new-laptop.ps1
@@ -24,3 +24,14 @@ New-Alias ll "get-childitem"
 ## gcp
 ##---------------------------------------
 import-module googlecloud
+
+##---------------------------------------
+## colours for when using "Solarized Light" Windows Terminal theme
+##---------------------------------------
+set-psreadlineoption -Colors @{"Member"=[ConsoleColor]::Magenta}
+set-psreadlineoption -Colors @{"Parameter"=[ConsoleColor]::DarkMagenta}
+
+##---------------------------------------
+## useful snippet for seeing what colours look like in your current Windows Terminal theme
+##---------------------------------------
+# [Enum]::GetValues([ConsoleColor]) | ForEach-Object { Write-Host "$_" -ForegroundColor $_ }
